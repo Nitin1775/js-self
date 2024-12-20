00b162bcf14253eb76666fed94990b137cb8cf53
Nitin Shukla <164005274+Nitin1775@users.noreply.github.com>
2024-12-20 16:04:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";   // treat all JS code as newer version
+
+//alert( 3+3) // we are using node js not browser
+
+console.log( 3+3)
+//code readability should be high
+
+console.log("nitin")
+
+let name = "nitin"
+let age = 20
+let isLoggedIn = false 
+
+
+//number => 2 to the power 53
+// big int 
+// string => ""
+// boolean  => true/false
+// null => standalone value
+// undefined => 
+// symbol => used in case of uniqueness
+
+//object
+
+console.log(typeof null ) // output =>object
+console.log(typeof undefined )  //output =>undefined 
