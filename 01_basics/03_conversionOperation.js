e769cbc01761353f9bc09663b5ae0c81939cb540
Nitin Shukla <164005274+Nitin1775@users.noreply.github.com>
2024-12-20 16:41:41 +0000

    conversionoperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "43abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "43abc" => NaN
+// true => 1 ; false => 0
