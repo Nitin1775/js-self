797e943a59c9add3eb76b6670a02efac2a72bdea
Nitin Shukla <164005274+Nitin1775@users.noreply.github.com>
2024-12-22 09:19:50 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,3 +10,23 @@ console.log(valueInNumber);
 //"33" => 33
 // "43abc" => NaN
 // true => 1 ; false => 0
+
+
+
+let IsLoggedIn = "ff"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// ""=> false
+// "nitin" =>true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof(stringNumber));
