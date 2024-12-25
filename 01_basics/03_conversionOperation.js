ff2dc22a5c4ff0366cebe42bd77b6d6ce260ee24
Nitin Shukla <164005274+Nitin1775@users.noreply.github.com>
2024-12-25 13:00:23 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,11 +53,24 @@ let negValue = -value
 
 // console.log(str3);
 
-console.log("1" + 2);
-console.log(1 +"2");
-console.log("1" +"2");
-console.log("1" +2+2);
-console.log(1+2+"2");
+// console.log("1" + 2);
+// console.log(1 +"2");
+// console.log("1" +"2");
+// console.log("1" +2+2);
+// console.log(1+2+"2");
 
 
-console.log( (3+4)*5 %3);
+// console.log( (3+4)*5 %3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
