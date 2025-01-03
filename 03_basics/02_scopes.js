bc11a0fb444a1f8ccf42f5d95b996a150e6779bd
Nitin Shukla <164005274+Nitin1775@users.noreply.github.com>
2025-01-03 22:51:53 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    //var c = 30
+    console.log("INNER: ", a); //10 
+    
+}
+
+
+
+
+
+// console.log(a); //300
+// console.log(b);
+// console.log(c);
+
+
