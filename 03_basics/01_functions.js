d031a96e4d9adb85f48a01ca7ee7b9cf1d5e4eeb
Nitin Shukla <164005274+Nitin1775@users.noreply.github.com>
2025-01-03 22:38:18 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -47,4 +47,49 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("Nitin"));
-console.log(loginUserMessage("ni3"));
+//console.log(loginUserMessage("ni3"));
+
+
+//**************************next class**************************
+
+
+//rest operator and spread operator both has same symbol(...)
+// function calculateCartPrice(...num1){
+//     return num1
+// } //if we input 200,300,500 output is [ 200, 400, 500 ]
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+} // in this val1 = 200 and val2 = 400 and output is [ 500, 2000 ] 
+
+//console.log(calculateCartPrice(2));
+//console.log(calculateCartPrice(200, 400, 500,2000));
+
+const user = {
+    username: "Nitin",
+    price: 199
+}
+
+
+function handleObject(anyobject){
+     //console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+     
+}
+
+//handleObject(user)
+handleObject({
+    username: "ni3",
+    price: 399
+})
+
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 400, 100, 600]));
