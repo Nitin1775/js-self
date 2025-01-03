6bddf699fc63c49d995c2e8f867ca119ab4f0d69
Nitin Shukla <164005274+Nitin1775@users.noreply.github.com>
2025-01-03 13:48:22 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+// array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const myHeros = ["superman", "batman"]
+const myArray2 = new Array(1, 2, 3, 4)
+console.log(myArray[0]);
+
+
+
+//Array methods
+
+
+ 
