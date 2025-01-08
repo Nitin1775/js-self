3a345c0b5560cb2db482ac5e703ae3dac3593082
Nitin Shukla <164005274+Nitin1775@users.noreply.github.com>
2025-01-08 19:10:42 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,64 @@
+const user = {
+    username: "nitin",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+        
+        
+    }
+}
+
+
+
+// user.welcomeMessage() //nitin , welcome to website
+// user.username = "shukla"
+// user.welcomeMessage() //shukla , welcome to website
+
+// console.log(this);
+
+
+// function chai(){
+//     let username = "nitin"
+//     console.log(this.username) //undefined //we cannot use "this" in function  how we used in the object 
+// }
+
+
+// chai()
+
+
+// const chai = function () {
+//     let username = "Nitin"
+//     console.log(this.username);  //undefined
+     
+// }
+
+
+//*******arrow function**********
+// const chai =  () => {
+//     let username = "Nitin"
+//     console.log(this.username);  //undefined
+//     console.log(this); //{}
+// }
+// chai()
+
+
+
+// basic method
+// const addTwo = (num1, num2) =>{
+//     return num1 + num2
+// }
+
+//method 2
+// const addTwo = (num1, num2) => num1 + num2
+
+
+//method 3
+const addTwo = (num1, num2) =>  (num1 + num2)
+
+
+
+console.log(addTwo(3, 4));
+
+
